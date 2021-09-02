5d4efa66db170374148b8cbb5bc011a30f45720c
Austin Pocus <ajpocus@gmail.com>
2021-09-02 18:47:30 -0400

    man i should save more often

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginSass);
-  eleventyConfig.addPassthroughCopy('css/**/*.css');
+  eleventyConfig.addPassthroughCopy("css/**/*.css");
+  eleventyConfig.addPassthroughCopy("js/**/*.js");
 
   return {
     passthroughFileCopy: true
