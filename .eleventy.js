d669eddba3bdb30f4729ae0fbf16a4277376f31f
Austin Pocus <ajpocus@gmail.com>
2021-09-03 15:56:30 -0400

    sticky footer, pictures, stats, general style

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginSass);
   eleventyConfig.addPassthroughCopy("css/**/*.css");
   eleventyConfig.addPassthroughCopy("js/**/*.js");
+  eleventyConfig.addPassthroughCopy("img");
 
   return {
     passthroughFileCopy: true
--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const tooltip = document.querySelector(".stats-section .tooltip");
-    const aside = document.querySelector(".stats-section aside.info");
+    // const tooltip = document.querySelector(".stats-section .tooltip");
+    // const aside = document.querySelector(".stats-section aside.info");
 
-    tooltip.addEventListener("mouseenter", (ev) => {
-        aside.classList.remove("hidden");
-    });
+    // tooltip.addEventListener("mouseenter", (ev) => {
+    //     aside.classList.remove("hidden");
+    // });
 
-    tooltip.addEventListener("mouseout", (ev) => {
-        aside.classList.add("hidden");
-    });
+    // tooltip.addEventListener("mouseout", (ev) => {
+    //     aside.classList.add("hidden");
+    // });
 });
